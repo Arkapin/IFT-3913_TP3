240164ea6e2e0839b2ffbb30baaf5bb1543eb47e
PPHdUni <36003425+PPHdUni@users.noreply.github.com>
2022-04-08 20:22:11 -0400

    Mise à jour tests boite blanche

--- currency-converter-in-java-master/currency-converter-in-java-master/src/test/BoiteBlancheTests.java
+++ currency-converter-in-java-master/currency-converter-in-java-master/src/test/BoiteBlancheTests.java
@@ -10,6 +10,7 @@ import currencyConverter.Currency;
 import currencyConverter.MainWindow;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BoiteBlancheTests {
 
@@ -26,14 +27,28 @@ public class BoiteBlancheTests {
 
     @Test
     void testIChemins() {
+        Currency usDollar = currenciesIChemins.get(0);
+
         assertAll("I-chemins",
-                () -> testIChemin(200.0),
-                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1); testIChemin(12354.0);},
-                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1); testIChemin(636.0);},
-                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1); testIChemin(101.0);},
-                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1); testIChemin(66.0);},
-                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1); testIChemin(93.0);},
-                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1); testIChemin(100.0);},
+                () -> testIChemin(100.0*usDollar.getExchangeValues().get(currenciesIChemins.get(currenciesIChemins.size()-1).getShortName())),
+                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1);
+                testIChemin(100.0*usDollar.getExchangeValues().get(currenciesIChemins.get(currenciesIChemins.size()-1).getShortName()));
+                },
+                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1);
+                    testIChemin(100.0*usDollar.getExchangeValues().get(currenciesIChemins.get(currenciesIChemins.size()-1).getShortName()));
+                },
+                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1);
+                    testIChemin(100.0*usDollar.getExchangeValues().get(currenciesIChemins.get(currenciesIChemins.size()-1).getShortName()));
+                },
+                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1);
+                    testIChemin(100.0*usDollar.getExchangeValues().get(currenciesIChemins.get(currenciesIChemins.size()-1).getShortName()));
+                },
+                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1);
+                    testIChemin(100.0*usDollar.getExchangeValues().get(currenciesIChemins.get(currenciesIChemins.size()-1).getShortName()));
+                },
+                () -> {currenciesIChemins.remove(currenciesIChemins.size()-1);
+                    testIChemin(100.0*usDollar.getExchangeValues().get(currenciesIChemins.get(currenciesIChemins.size()-1).getShortName()));
+                },
                 () -> {currenciesIChemins.remove(currenciesIChemins.size()-1); testSkipBoucle();});
     }
 
@@ -44,27 +59,27 @@ public class BoiteBlancheTests {
 
         assertAll(currenciesIChemins.size()+"-chemin",
                 () -> assertEquals(expected, MainWindow.convert("US Dollar", name, currenciesIChemins, 100.0),
-                        "Échec du cas 1 pour "+currenciesIChemins.size()+"-chemins"),
+                        "Échec du cas 1 pour "+currenciesIChemins.size()+" itérations de boucle"),
                 () -> assertEquals(0.0, MainWindow.convert("NULL", name, currenciesIChemins, 100.0),
-                        "Échec du cas 2 pour "+currenciesIChemins.size()+"-chemins"),
+                        "Échec du cas 2 pour "+currenciesIChemins.size()+" itérations de boucle"),
                 () -> {
                     currencyIChemin.setShortName(null);
                     currenciesIChemins.set(currenciesIChemins.size()-1, currencyIChemin);
                     assertEquals(0.0, MainWindow.convert("US Dollar", name, currenciesIChemins, 100.0),
-                            "Échec du cas 3 pour "+currenciesIChemins.size()+"-chemins");
+                            "Échec du cas 3 pour "+currenciesIChemins.size()+" itérations de boucle");
                 },
                 () -> {
                     currencyIChemin.setShortName(shortName);
                     currenciesIChemins.set(currenciesIChemins.size()-1, currencyIChemin);
                     assertEquals(0.0, MainWindow.convert("US Dollar", "NULL", currenciesIChemins, 100.0),
-                            "Échec du cas 4 pour "+currenciesIChemins.size()+"-chemins");
+                            "Échec du cas 4 pour "+currenciesIChemins.size()+" itérations de boucle");
                 }
         );
     }
 
     void testSkipBoucle() {
         assertEquals(0.0, MainWindow.convert("US Dollar", "US Dollar", currenciesIChemins, 100.0),
-                "Échec du test pour skip boucle");
+                "Échec du test pour aucune itération de boucle");
     }
 
 }
